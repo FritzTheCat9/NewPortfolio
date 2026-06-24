f9a8af4072a9986651e2c5feab89a29a01c945e7
FritzTheCat9 <bartlomiejuminski1999@gmail.com>
2026-06-24 22:13:24 +0200

    tooltip on skill hover

--- new-portfolio/src/lib/components/SkillChip.svelte
+++ new-portfolio/src/lib/components/SkillChip.svelte
@@ -10,19 +10,47 @@
 </script>
 
 <div
-	class="flex items-center gap-2 px-3 py-1 rounded-full
+	class="inline-flex items-center gap-2 px-3 py-1 rounded-full
 	bg-slate-900 border border-slate-700
 	text-slate-300 text-sm
 	hover:bg-slate-800 hover:text-white
-	transition-all duration-200"
+	transition-colors duration-200"
 >
-	{#if icon}
-		<iconify-icon {icon} width="18" height="18"></iconify-icon>
-	{:else if image}
-		<img src={image} alt={name} class="w-4 h-4 object-contain" />
-	{/if}
+	<!-- ICON ONLY IS THE HOVER TRIGGER -->
+	<span class="relative group/icon flex items-center">
+		{#if icon}
+			<iconify-icon {icon} width="18" height="18"></iconify-icon>
+		{:else if image}
+			<img src={image} alt={name} class="w-4 h-4 object-contain" />
+		{/if}
+
+		{#if !showName}
+			<!-- tooltip -->
+			<span
+				class="
+					absolute left-1/2 -translate-x-1/2 -top-9
+					px-2 py-1 rounded-md
+					bg-slate-950 border border-slate-700
+					text-xs text-slate-200
+					whitespace-nowrap
+					opacity-0 scale-95 translate-y-1
+					group-hover/icon:opacity-100
+					group-hover/icon:scale-100
+					group-hover/icon:translate-y-0
+					transition-all duration-200 ease-out
+					delay-150
+					pointer-events-none
+					z-50
+				"
+			>
+				{name}
+			</span>
+		{/if}
+	</span>
 
 	{#if showName}
-		<span>{name}</span>
+		<span class="whitespace-nowrap">
+			{name}
+		</span>
 	{/if}
 </div>
