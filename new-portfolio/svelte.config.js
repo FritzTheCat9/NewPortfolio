c0fac86d299a438360d43d346969dd90ab1b351b
FritzTheCat9 <bartlomiejuminski1999@gmail.com>
2026-06-28 18:59:33 +0200

    fix

--- new-portfolio/svelte.config.js
+++ new-portfolio/svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+
 export default {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		})
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : ''
+		}
 	}
 };
